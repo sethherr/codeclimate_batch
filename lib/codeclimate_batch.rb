16430c0116624623876b46824a82097cd1653465
sethherr <seth.william.herr@gmail.com>
2016-11-09 19:41:25 -0800

    reduce to merge

--- lib/codeclimate_batch.rb
+++ lib/codeclimate_batch.rb
@@ -5,22 +5,9 @@ module CodeclimateBatch
     # Start TestReporter with appropriate settings.
     # Note that Code Climate only accepts reports from the default branch (usually master, but can be changed)
     # but records coverage on all PRs and displays their coverage difference
-    def start
-      return if travis? && (outside_default_branch? && !pull_request?)
-      ENV['CODECLIMATE_TO_FILE'] = '1' # write results to file since we need to combine them before sending
-      require 'codeclimate-test-reporter'
-      CodeClimate::TestReporter.start
-    end
 
     def unify(coverage_files)
-      initial, *rest = coverage_files
-      report = load(initial)
-      rest.each do |file|
-        report.merge!(load(file))
-        # merge_source_files(report.fetch("source_files"), load(file).fetch("source_files"))
-      end
-      # recalculate_counters(report)
-      report
+      ([{}] + coverage_files).reduce { |hash, path| hash.merge(load(path)) }
     end
 
     private
@@ -36,56 +23,8 @@ module CodeclimateBatch
       ENV['TRAVIS']
     end
 
-    # Check if our Travis build is running on the default branch.
-    def outside_default_branch?
-      default_branch != ENV['TRAVIS_BRANCH']
-    end
-
-    # Check if running a pull request.
-    def pull_request?
-      ENV['TRAVIS_PULL_REQUEST'].to_i != 0
-    end
-
     def load(file)
       JSON.load(File.read(file))
-    end
-
-    def recalculate_counters(report)
-      source_files = report.fetch("source_files").map { |s| s["line_counts"] }
-      report["line_counts"].keys.each do |k|
-        report["line_counts"][k] = source_files.map { |s| s[k] }.inject(:+)
-      end
-    end
-
-    def merge_source_files(all, source_files)
-      source_files.each do |new_file|
-        old_file = all.detect { |source_file| source_file["name"] == new_file["name"] }
-
-        if old_file
-          # merge source files
-          coverage = merge_coverage(
-            JSON.load(new_file.fetch("coverage")),
-            JSON.load(old_file.fetch("coverage"))
-          )
-          old_file["coverage"] = JSON.dump(coverage)
-
-          total = coverage.size
-          missed, covered = coverage.compact.partition { |l| l == 0 }.map(&:size)
-          old_file["covered_percent"] = (covered == 0 ? 0.0 : covered * 100.0 / (covered + missed))
-          old_file["line_counts"] = {"total" => total, "covered" => covered, "missed" => missed}
-        else
-          # just use the new value
-          all << new_file
-        end
-      end
-    end
-
-    # [nil,1,0] + [nil,nil,2] -> [nil,1,2]
-    def merge_coverage(a,b)
-      b.map! do |b_count|
-        a_count = a.shift
-        (!b_count && !a_count) ? nil : b_count.to_i + a_count.to_i
-      end
     end
   end
 end
--- spec/codeclimate_batch_spec.rb
+++ spec/codeclimate_batch_spec.rb
@@ -13,136 +13,17 @@ describe CodeclimateBatch do
     CodeclimateBatch::VERSION.should =~ /^[\.\da-z]+$/
   end
 
-  describe "CLI" do
-    def sh(command, options={})
-      result = `#{command} #{"2>&1" unless options[:keep_output]}`
-      raise "#{options[:fail] ? "SUCCESS" : "FAIL"} #{command}\n#{result}" if $?.success? == !!options[:fail]
-      result
-    end
-
-    def batch(command, options={})
-      sh("#{Bundler.root}/bin/codeclimate-batch #{command}", options)
-    end
-
-    it "shows --version" do
-      batch("--version").should include(CodeclimateBatch::VERSION)
-    end
-
-    it "shows --help" do
-      batch("--help").should include("codeclimate")
-    end
-
-    it "does nothing when there are no files" do
-      batch("--groups 4").should == "Code climate: No files found to report\n"
-    end
-
-    it "merges and reports" do
-      with_env("TRAVIS_REPO_SLUG" => "xxx/yyy", "TRAVIS_BUILD_NUMBER" => rand(900000).to_s) do
-        base = "#{Dir.tmpdir}/codeclimate-test-coverage-"
-
-        # pretend we just ran code climate reporter
-        ["report_a.json", "report_b.json"].each do |r|
-          sh "cp #{Bundler.root}/spec/files/#{r} #{base}#{r}"
-        end
-        Dir["#{base}*"].size.should == 2
-
-        # send reports
-        result = batch("--groups 4")
-        result.should include "waiting for 3/4 reports on xxx-yyy-"
-        result.sub(/\d+\.\d+s/,'TIME').should include "Code climate: TIME to send 2 reports"
-
-        # all cleaned up ?
-        Dir["#{base}*"].size.should == 0
-      end
-    end
-  end
-
-  describe ".start" do
-    let(:default) {{"TRAVIS" => "1", "TRAVIS_BRANCH" => "master", "CODECLIMATE_TO_FILE" => nil, "TRAVIS_PULL_REQUEST" => nil}}
-
-    it "calls start when on travis master" do
-      with_env(default) do
-        CodeClimate::TestReporter.should_receive(:start)
-        CodeclimateBatch.start
-        ENV["CODECLIMATE_TO_FILE"].should == "1"
-      end
-    end
-
-    it "starts without travis since we don't know how to handle other cis" do
-      default.delete("TRAVIS")
-      with_env(default) do
-        CodeClimate::TestReporter.should_receive(:start)
-        CodeclimateBatch.start
-      end
-    end
-
-    it "does not start on different branch" do
-      default["TRAVIS_BRANCH"] = "mooo"
-      with_env(default) do
-        CodeClimate::TestReporter.should_not_receive(:start)
-        CodeclimateBatch.start
-      end
-    end
-
-    it "starts on different branch if set as default branch" do
-      default.merge! "TRAVIS_BRANCH" => "moooo", "DEFAULT_BRANCH" => "moooo"
-      with_env(default) do
-        CodeClimate::TestReporter.should_receive(:start)
-        CodeclimateBatch.start
-      end
-    end
-
-    it "does not starts on different branch if it doesn't match default branch" do
-      default.merge! "TRAVIS_BRANCH" => "moooo", "DEFAULT_BRANCH" => "monster"
-      with_env(default) do
-        CodeClimate::TestReporter.should_not_receive(:start)
-        CodeclimateBatch.start
-      end
-    end
-
-    it "starts on PR" do
-      default["TRAVIS_PULL_REQUEST"] = "123"
-      with_env(default) do
-        CodeClimate::TestReporter.should_receive(:start)
-        CodeclimateBatch.start
-      end
-    end
-  end
-
   describe ".unify" do
     it "merges reports 1 report" do
       report = CodeclimateBatch.unify(["spec/files/report_a.json"])
 
-      report.should == {"Unit Tests":{"coverage":{"/sample.rb":[1,1,1],"/another_file.rb":[1,1,1]}}}
+      report.should == JSON.parse('{"Unit Tests":{"coverage":{"/sample.rb":[1,1,1],"/another_file.rb":[1,1,1]}}}')
     end
 
     it "merges multiple reports" do
       report = CodeclimateBatch.unify(["spec/files/report_a.json", "spec/files/report_b.json"])
 
-      report.should == {"Unit Tests":{"coverage":{"/sample.rb":[1,1,2],"/another_file.rb":[1,2,2]}}}
-    end
-  end
-
-  describe ".merge_source_files" do
-    it "merges" do
-      all = [{"name" => "a.rb", "coverage" => '[null,1,null]'}, {"name" => "b.rb", "coverage" => '[1,1,null]'}]
-      CodeclimateBatch.send(:merge_source_files,
-        all,
-        [{"name" => "b.rb", "coverage" => '[null,2,1]'}]
-      )
-      all.should == [
-        {"name"=>"a.rb", "coverage"=>"[null,1,null]"},
-        {"name"=>"b.rb", "coverage"=>"[1,3,1]", "covered_percent"=>100.0, "line_counts"=>{"total"=>3, "covered"=>3, "missed"=>0}}
-      ]
-    end
-
-    it "merges uncovered" do
-      all = [{"name" => "a.rb", "coverage" => '[]'}]
-      CodeclimateBatch.send(:merge_source_files,
-        all,
-        all
-      )
-      all.should == [{"name"=>"a.rb", "coverage"=>"[]", "covered_percent"=>0.0, "line_counts"=>{"total"=>0, "covered"=>0, "missed"=>0}}]
+      report.should == JSON.parse('{"Unit Tests":{"coverage":{"/sample.rb":[1,1,1],"/another_file.rb":[1,1,1]}}, "Integration Tests":{"coverage":{"/sample.rb":[null,0,1],"/another_file.rb":[0,1,1]}}}')
     end
   end
 end
